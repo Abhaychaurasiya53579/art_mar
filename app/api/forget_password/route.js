7cfff88e70359b1eaac2e5ed94a4e1da0607b681
Abhaychaurasiya53579 <97787240+Abhaychaurasiya53579@users.noreply.github.com>
2024-07-06 17:17:58 +0530

    dj

--- app/api/forget_password/route.js
+++ app/api/forget_password/route.js
@@ -38,7 +38,7 @@ export async function POST(request) {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n`
         + `Please click on the following link, or paste this into your browser to complete the process:\n\n`
-        + `https://art-market-omega.vercel.app/reset-password/${reset_token}\n\n`
+        + `https://art-market-omega.vercel.app/reset_password/${reset_token}\n\n`
         + `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
